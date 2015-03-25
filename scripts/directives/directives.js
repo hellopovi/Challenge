e12a4502041a43d479e615f3319eb9783cd860f7
Povilas <povi@userreport.com>
2015-03-25 19:51:10 +0100

    Further improvements

--- scripts/directives/directives.js
+++ scripts/directives/directives.js
@@ -1,3 +1,23 @@
+nordeaApp.directive('appIcon', ['$animate', function($animate) {
+	return {
+		restrict: 'A',
+		scope: true,
+		link: function(scope, element, attrs) {
+			var closeButton;
+			var appType = attrs.appIcon;
+			scope.$watch(element.children().length, function() {
+				closeButton = element.children()[0];
+			});
+			element.bind('click', function (e) {
+				if(appType != "default"){
+					angular.element(closeButton).toggleClass('ng-hide');
+					element.toggleClass('animated infinite pulse');
+				}
+			});
+		}
+	};
+}]);
+
 nordeaApp.directive('deleteAppIcon', function() {
 	return {
 		restrict: 'A',
